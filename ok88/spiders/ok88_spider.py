7a0a00326ba52f94883bd029e4cf65e30d3aa918
yangzhihuilzy <yangzhihuilzy@sina.com>
2017-07-27 16:49:01 +0800

    Get all links

---
+++ ok88/spiders/ok88_spider.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class Ok88Spider(scrapy.Spider):
+
+    name = "ok88spider"
+    allowed_domains = ["ok88ok88.com"]
+    start_urls = ["http://www.ok88ok88.com/read.php?tid=22159"]
+
+    def parse(self, response):
+
+        #a[contains(@href, "image")]
+        #all_links = response.xpath('//a/@href')
+        #driver.find_element_by_xpath("//a[contains(@id,'url_165')]")
+        all_links = response.xpath('//a[contains(@id,"url_")]/text()')
+
+        for link in all_links:
+            print link.extract()
+            print  link.re(r'http://w{0,3}\.?ok88ok88.com/read\.php\?tid=\d{2,5}$')
+            #print link.re(r"http://www.ok88ok88.com/read.php?tid=.*")
+            # if link.re(r"http://www.ok88ok88.com/read.php?tid=.*"):
+            #     print link.extract()
+            # else:
+            #     print "Not a needed page!"
