4351e345689e977a9e1de95f59cd636282c555ec
yangzhihuilzy <yangzhihuilzy@sina.com>
2017-07-27 18:41:14 +0800

    track links ok

--- ok88/spiders/ok88_spider.py
+++ ok88/spiders/ok88_spider.py
@@ -14,10 +14,13 @@ class Ok88Spider(scrapy.Spider):
         all_links = response.xpath('//a[contains(@id,"url_")]/text()')
 
         for link in all_links:
-            print link.extract()
-            print  link.re(r'http://w{0,3}\.?ok88ok88.com/read\.php\?tid=\d{2,5}$')
+            #print link.extract()
+            if  link.re(r'http://w{0,3}\.?ok88ok88.com/read\.php\?tid=\d{2,5}$') is not None:
+                yield scrapy.Request(link.extract(), callback=self.parse_dir_contents)
             #print link.re(r"http://www.ok88ok88.com/read.php?tid=.*")
             # if link.re(r"http://www.ok88ok88.com/read.php?tid=.*"):
             #     print link.extract()
             # else:
             #     print "Not a needed page!"
+    def parse_dir_contents(self, response):
+        print "get url" + response.url
